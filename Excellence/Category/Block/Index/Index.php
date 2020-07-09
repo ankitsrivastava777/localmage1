5dd1c37d85da0e96ac0c62445c610a290a473f4d
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-07-09 12:05:36 +0530

    show and hide category as per toggel in category

---
+++ Excellence/Category/Block/Index/Index.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Excellence\Category\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+        /**
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    protected $_categoryFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collecionFactory,
+
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_categoryFactory = $collecionFactory;
+
+    }
+
+    public function getWebsites() {
+        return $this->_storeManager->getWebsites();
+    }
+    public function getCurrentWebsiteId()
+    {
+        return $this->_storeManager->getWebsite()->getId();
+    }
+    public function getCategoryCollection() {
+        $collection = $this->_categoryFactory->create();
+        $collection->addAttributeToSelect('*');
+        return $collection;
+    }
+    public function getCategoryAttribute($id)
+    {
+        $collection = $this->_categoryFactory
+                        ->create()
+                        ->addAttributeToSelect('*')
+                        ->addAttributeToFilter('entity_id',['eq'=>$id])
+                        ->setPageSize(1);
+    
+       return $collection->getFirstItem();
+    
+    }
+}
+
