368991808f9e5648eb29e28aeceb50bfa22f41ec
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-07-11 18:19:00 +0530

    compleated customer access a/c to attribute value.

--- Excellence/Category/Observer/CheckLoginPersistentObserver.php
+++ Excellence/Category/Observer/CheckLoginPersistentObserver.php
@@ -11,6 +11,8 @@ class CheckLoginPersistentObserver implements ObserverInterface
      * @var \Magento\Framework\UrlInterface
      */
     protected $url;
+    protected $customerRepositoryInterface;
+    protected $_urlInterface;
 
     /**
      * @var \Magento\Framework\App\Response\Http
@@ -30,13 +32,17 @@ class CheckLoginPersistentObserver implements ObserverInterface
         \Magento\Framework\UrlInterface $url,
         \Magento\Framework\App\Response\Http $http,
         \Magento\Customer\Model\Session $customerSession,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface,
+        \Magento\Framework\UrlInterface $urlInterface
 
     ) {
         $this->url = $url;
         $this->http = $http;
         $this->customerSession = $customerSession;
         $this->_storeManager = $storeManager;
+        $this->customerRepositoryInterface = $customerRepositoryInterface;
+        $this->_urlInterface = $urlInterface;
     }
 
     /**
@@ -44,20 +50,32 @@ class CheckLoginPersistentObserver implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        $currentWebsiteId = $this->_storeManager->getStore()->getWebsiteId();
-
-        if ($currentWebsiteId != "1") {
+        $customerId = $this->customerSession->getId();
+        if ($customerId) {
+            $customer = $this->customerRepositoryInterface->getById($customerId);
+            $customerAttr = $customer->getCustomAttribute('accesswebsite')->getValue();
+            $currentUrl = $this->_urlInterface->getCurrentUrl() . "/";
             /**
              * Check if user logged in
              */
-            if ($this->customerSession->isLoggedIn()) {
-                return;
+            if ($this->customerSession->isLoggedIn() && $customerAttr == "0") {
+
+                if ($currentUrl != $this->url->getUrl('access-denied')) {
+                    $this->http->setRedirect($this->url->getUrl('access-denied'), 301);
+                }
             }
-            if ($observer->getRequest()->getFullActionName() == 'cms_index_index') {
-                /**
-                 * Redirect to login
-                 */
-                $this->http->setRedirect($this->url->getUrl('customer/account/login'), 301);
+        }
+        if (!$customerId) {
+            $currentUrl = $this->_urlInterface->getCurrentUrl();
+            /**
+             * Check if user logged in
+             */
+
+            if (!$this->customerSession->isLoggedIn()) {
+
+                if ($currentUrl != $this->url->getUrl('customer/account/login')) {
+                    $this->http->setRedirect($this->url->getUrl('customer/account/login'), 301);
+                }
             }
         }
     }
